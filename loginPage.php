88ffc988380e5cf6189c4c65a67f32c0c30d0b9b
Dan <dh954630@gmail.com>
2017-07-01 21:05:35 -0400

    got the login page and backend working. actually queries database and does stuff

---
+++ loginPage.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://getbootstrap.com/docs-assets/ico/favicon.png">
+
+    <title>Knight Events Login</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://getbootstrap.com/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <style>
+    body {
+      padding-top: 0px;
+      padding-bottom: 40px;
+      background-color: #ffc904;
+    }
+    .form-signin {
+      max-width: 330px;
+      padding: 15px;
+      margin: 0 auto;
+    }
+    .form-signin .form-signin-heading,
+    .form-signin .checkbox {
+      margin-bottom: 10px;
+    }
+    .form-signin .checkbox {
+      font-weight: normal;
+    }
+    .form-signin .form-control {
+      position: relative;
+      font-size: 16px;
+      height: auto;
+      padding: 10px;
+      -webkit-box-sizing: border-box;
+         -moz-box-sizing: border-box;
+              box-sizing: border-box;
+    }
+    .form-signin .form-control:focus {
+      z-index: 2;
+    }
+    .form-signin input[type="text"] {
+      margin-bottom: -1px;
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+    .form-signin input[type="password"] {
+      margin-bottom: 10px;
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
+    </style>
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="http://getbootstrap.com/docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Knight Events</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+    </ul>
+  </div>
+</nav>
+
+    <div class="container">
+      <form class="form-signin">
+	  <img src="KnightEventsLogo.jpg" class="img-rounded" alt="UCF Logo" width="304" height="236">
+        <h2 class="form-signin-heading">Please sign in</h2>
+		
+		<form action="submitLogin.php" method="post">		
+				<input type="text" class="form-control" id = "username" name = "username" placeholder="Email address" required autofocus>
+				<input type="password" class="form-control" id = "password" name = "password" placeholder="Password" required>
+				<label class="checkbox">
+				<input type="checkbox" value="remember-me"> Remember me
+				</label>
+				<button class="btn btn-lg btn-primary btn-block" type="submit"><a href="submitLogin.php">Sign in</a></button>
+		</form>
+		<p align="center">Click <a href="login4.html">Here</a> to register</p>
+      </form>
+	  
+	  <div class="space"> </div>
+	  
+	  
+
+    </div> <!-- /container -->
+	
+	
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
---
+++ submitLogin.php
@@ -0,0 +1,91 @@
+<?php 
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$username2 = "Dan";
+$password2 = "password1";
+//a defualt user_id so we know when it fails
+$user_id = "Default";
+//error message variable
+$message = "no message";
+
+echo($username2);
+echo($password2);
+
+/*** begin our session ***/
+session_start();
+
+
+/*** check if the users is already logged in ***/
+if(isset( $_SESSION['user_id'] ))
+{
+    $message = 'User is already logged in';
+}
+/*** check that both the username, password have been submitted ***/
+if(!isset( $_POST['username'], $_POST['password']))
+{
+    $message = 'Please enter a valid username and password';
+}
+
+ /*** connect to database ***/
+    /*** mysql hostname ***/
+    $mysql_hostname = 'localhost';
+    /*** mysql username ***/
+    $mysql_username = 'root';
+    /*** mysql password ***/
+    $mysql_password = 'okay';
+    /*** database name ***/
+    $mysql_dbname = 'cop4710';
+	
+// Create connection
+$conn = mysqli_connect($mysql_hostname, $mysql_username, $mysql_password, $mysql_dbname);
+
+// Check connection
+if (!$conn) 
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+ /*prepare the select statement*/
+$result = $conn->query("SELECT user_id, username, password, priv FROM users WHERE username = '$username' AND password = '$password'");
+
+while($row = mysqli_fetch_array($result))
+{
+	$user_id = $row['user_id'];
+	$privilege = $row["priv"];
+	echo "'$user_id'";
+	echo "'$privilege'";
+	
+}
+
+//if they failed to login
+if($user_id == "Default")
+        {
+                $message = 'Failure';
+        }
+if($user_id == false)
+        {
+                $message = 'Failure';
+        }
+ /*We got the data that we need. now we will use it*/
+        else
+        {
+                /*set session user_id and privilege number vars*/
+                $_SESSION["user_id"] = $user_id;
+				$_SESSION["user_priv"] = $privilege;
+				
+				
+                /* log in was successfull.go to home page */                
+				header('Location: events.html');
+				
+        }
+
+
+
+
+  
+
+?>
+
+
