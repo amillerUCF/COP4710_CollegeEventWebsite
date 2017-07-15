ae5923fb1d296dcd220184d88561954ccc097717
Dan <dh954630@gmail.com>
2017-07-15 15:40:01 -0400

    updated

--- dbscript.sql
+++ dbscript.sql
@@ -1,7 +1,3 @@
-# SQL commands to create and populate the MySQL database for
-# COP 4710 - Spring 2015
-#
-# delete the database if it already exists
 drop database if exists cop4710;
 
 #create a new database named cop4710
@@ -77,6 +73,8 @@ CREATE TABLE rso(
 	admin			INT(11)
 );
 
+ALTER TABLE rso ADD COLUMN name VARCHAR(15) AFTER rso_id;
+
 #create the Follows table
 #table is for when a user follows an event.
 CREATE TABLE follows (
@@ -85,69 +83,12 @@ CREATE TABLE follows (
     PRIMARY KEY (user_id, event_id)
 );
 
-
-INSERT INTO users (username, password, priv, firstname, lastname, email, reg_date, univ_id, rso) VALUES ("superAdmin", "dc76e9f0c0006e8f919e0c515c66dbba3982f785", 1, "super", "admin", "example@ucf.edu", NOW(), 1, 0);
-INSERT INTO users (username, password, priv, firstname, lastname, email, reg_date, univ_id, rso) VALUES ("admin", "dc76e9f0c0006e8f919e0c515c66dbba3982f785", 2, "admin", " ", "example@knights.ucf.edu", NOW(), 1, 1);
-INSERT INTO users (username, password, priv, firstname, lastname, email, reg_date, univ_id, rso) VALUES ("user", "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8", 3, "user", "name", "example@knights.ucf.edu", NOW(), 1, 0);
-INSERT INTO users (username, password, priv, firstname, lastname, email, reg_date, univ_id, rso) VALUES ("brockstoops", "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8", 3, "Brock", "Stoops", "brockstoops@knights.ucf.edu", NOW(), 1, 1);
-INSERT INTO users (username, password, priv, firstname, lastname, email, reg_date, univ_id, rso) VALUES ("michaelschnell", "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8", 3, "michael", "schnell", "mschell@knights.ucf.edu", NOW(), 1, 1);
-INSERT INTO universities (name, location, description, num_students, pictures) VALUES("UCF", "Orlando", "University of Central Florida", 56000, null);
-INSERT INTO universities (name, location, description, num_students, pictures) VALUES("UCB", "Berkeley", "University of California - Berkeley", 15000, null);
-INSERT INTO universities (name, location, description, num_students, pictures) VALUES("USF", "Tampa", "University of South Florida", 30000, null);
-INSERT INTO events (name, category, description, event_time, event_date, location, univ_id, priv, rso, contact_phone, contact_email) VALUES ("COP4710", "class", "Database Management Systems", "14:00:00", "2015-04-15", "University of Central Florida", 1, 0, 0, "4071234567", "andres.vargas@knights.ucf.edu");
-INSERT INTO events (name, category, description, event_time, event_date, location, univ_id, priv, rso, contact_phone, contact_email) VALUES ("COP4710 Presentation", "class", "Database Management Systems", "14:30:00", "2015-04-16", "University of Central Florida", 1, 1, 0, "4071234567", "andres.vargas@knights.ucf.edu"); 
-INSERT INTO events (name, category, description, event_time, event_date, location, univ_id, priv, rso, contact_phone, contact_email) VALUES ("COP4516", "class", "Team Dynamics", "09:00:00", "2015-04-17", "University of Central Florida", 1, 0, 0, "4071234567", "programmer@knights.ucf.edu");
-INSERT INTO events (name, category, description, event_time, event_date, location, univ_id, priv, rso, contact_phone, contact_email) VALUES ("CS61B", "class", "Data Structures", "12:30:00", "2015-04-16", "University of California Berkeley", 2, 0, 0, "4071234567", "andres.vargas@calmail.berkeley.edu"); 
-INSERT INTO events (name, category, description, event_time, event_date, location, univ_id, priv, rso, contact_phone, contact_email) VALUES ("CS70", "class", "Discrete Math and Probability", "12:30:00", "2015-04-16", "University of California Berkeley", 2, 1, 0, "4071234567", "andres.vargas@calmail.berkeley.edu"); 
-INSERT INTO events (name, category, description, event_time, event_date, location, univ_id, priv, rso, contact_phone, contact_email) VALUES ("MAC 2312", "class", "Calculus 2", "15:30:00", "2015-04-17", "University of South Florida", 3, 0, 0, "4071234567", "andres.vargas@bulls.usf.edu");
-INSERT INTO events (name, category, description, event_time, event_date, location, univ_id, priv, rso, contact_phone, contact_email) VALUES ("COP4710 Meeting", "class", "Database Management System Group Meeting", "17:30:00", "2015-04-17", "University of Central Florida", 1, 2, 1, "4071234567", "brockstoops@knights.ucf.edu");
-INSERT INTO rso (email, admin) VALUES ("knights.ucf.edu", 1);
-INSERT INTO comments (event_id, text) VALUES (2, "this presentation is awesome");
-INSERT INTO comments (event_id, text) VALUES (2, "this presentation is okay");
-INSERT INTO comments (event_id, text) VALUES (2, "this presentation is bad");
-INSERT INTO ratings (event_id, rating, comment_id) VALUES (2, 5, 1);
-INSERT INTO ratings (event_id, rating, comment_id) VALUES (2, 4, 2);
-INSERT INTO ratings (event_id, rating, comment_id) VALUES (2, 2,3);
-
-
-# Returns events at users university
-#need to input the user id
-#returns both public and private events
-#SELECT e.* FROM events e, universities u, users s WHERE s.univ_id = u.univ_id AND u.location = e.location AND s.user_id = (?)
-
-# Selects all public events from a specific university
-#need to input the location of a university
-#SELECT e.* FROM events e, universities u WHERE e.location = u.location AND e.priv = 0 AND u.location = (?)
-
-# Selects all public events from a university
-#need to input the id of a university
-#SELECT e.* FROM events e, universities u WHERE e.location = u.location AND e.priv = 0 AND u.univ_id = (?)
-
-#events in a users RSO
-#need to input the user id to return the events
-#SELECT e.* FROM events e, users s WHERE s.RSO = e.RSO AND s.user_id = (?)
-
-#public event below
-#returns all the public events regardless of location
-#SELECT * FROM events WHERE priv = 0
-
-#Returns the private events at a users university
-#need to input the user id
-#SELECT e.* FROM events e, universities u, users s WHERE s.univ_id = u.univ_id AND u.location = e.location AND e.priv = 1 AND s.user_id = (?)
-
-#Selects all the comments for the event 
-#need to input the event id to get the comments
-#SELECT c.text FROM comments c, events e WHERE e.event_id = c.event_id AND e.event_id = (?)
-
-#Selects all the ratings from an event
-#need to input the event id to get the ratings.
-#SELECT r.rating FROM ratings r, events e WHERE e.event_id = r.rating AND e.event_id = (?)
-
-#Selects both comment and rating on an event
-#need to input the event id
-SELECT c.text, r.rating FROM comments c, ratings r, events e WHERE e.event_id = c.event_id AND r.comment_id = c.comment_id AND e.event_id = 2
-
-#Should return all the available events for a user to see.
-#combines multiple queries
-#need to input user id
-#SELECT DISTINCT e.* FROM events e, users u WHERE u.user_id = (?) AND ((e.priv = 0) OR (e.priv = 1 AND u.univ_id = e.univ_id) OR (e.priv = 2 AND u.rso = e.rso))
+INSERT INTO rso (name, email, admin) VALUES ("KnightsMMA","dhellkamp@knights.ucf.edu", 1);
+INSERT INTO rso (name, email, admin) VALUES ("Hack@UCF","dhellkamp@knights.ucf.edu", 1);
+INSERT INTO rso (name, email, admin) VALUES ("TechKnights","dhellkamp@knights.ucf.edu", 1);
+INSERT INTO rso (name, email, admin) VALUES ("VideoGameKnights","dhellkamp@knights.ucf.edu", 1);
+INSERT INTO events (event_id, name, category, description, event_time, event_date, location, univ_id, priv, rso, contact_phone, contact_email) VALUES (000001, "Presentation on SQLi", "workshop", "Attacking Databases muhuhaha", "16:00:00", "2017-07-15", "University of Central Florida", 1, 0, 3, "4071234567", "dhellkamp@knights.ucf.edu");
+INSERT INTO follows (user_id, event_id) VALUES (Default, 1);
+INSERT INTO follows (user_id, event_id) VALUES (Default, 8);
+INSERT INTO follows (user_id, event_id) VALUES (123456, 1);
+INSERT INTO follows (user_id, event_id) VALUES (123456, 1);
