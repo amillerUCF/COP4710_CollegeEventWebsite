ce486c5480f414fa19397a6c6b709f8631e4ca4a
Austin <amiller1@knights.ucf.edu>
2017-07-15 15:31:12 -0400

    Share FB link added to "eventInfo.php"

--- eventInfo.php
+++ eventInfo.php
@@ -7,6 +7,16 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="http://getbootstrap.com/docs-assets/ico/favicon.png">
     
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    
     <title>Your Events</title>
   
     <!-- Bootstrap core CSS -->
@@ -144,6 +154,12 @@
                 <h5>Description: <p><?php echo $description ?></p></h5>
             </p1>
             <br>
+            
+            <!-- Facebook share button code -->
+            <button><div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a></div></button>
+              
+            <br>
+            
             <h4>Comments</h4>
             
             <hr>
